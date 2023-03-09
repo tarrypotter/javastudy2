63fce3ceb5adce00e63ac0807402e6f54b0b8990
LEE TaeWonng <109096999+tarrypotter@users.noreply.github.com>
2023-03-09 22:55:45 +0000

    Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
-//import java.util.Scanner;
+//자바 모음
+
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
 
 // 변수 선언
     int a=5 ,b=9;
-
-    System.out.println("Hello world!");   
+// 문자열 
+    System.out.println("Hello world!");
+// 칸 띄우기
+    System.out.println();
+// 덧셈 
     System.out.println(a+b);
+    System.out.println();
+    
 // 배열   
     int[] i = new int[3];
     i[0]=3;
@@ -17,8 +24,9 @@ public class Main {
     System.out.println(i[1]);
     System.out.println(i[2]);
   System.out.println(i[0]+i[1]+i[2]);
+  System.out.println();
+    
 
-/*
 // 스캐너 
     Scanner scanner = new Scanner(System.in);
         
@@ -35,11 +43,22 @@ public class Main {
         System.out.println("당신의 나이는 "+ age +"입니다");
 
         scanner.close();
-   */
+
     
 //반복문
    for(int c=1; c<=9; c++){
       System.out.println(c);
+    }
+     System.out.println();
+
+//조건문 
+    int d=99;
+    if(d % 2 == 1) {
+      System.out.println("홀수");
+    }
+    else{
+     System.out.println("짝수"); 
+     System.out.println();
     }
   }
 }
